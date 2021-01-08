db95d8cb7b4e02765afdb1320fcb1efa7a53ec88
Ibrahima Biram Diouf <ibrahimabiram@gmail.com>
2021-01-08 16:49:12 +0000

    Feature/static pages

--- pages/about.js
+++ pages/about.js
@@ -399,7 +399,10 @@ export default function about() {
               >
                 <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                   <div className="px-4 py-5 flex-auto">
-                    <div className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-gray-600">
+                    <div
+                      className="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full"
+                      style={{ backgroundColor: 'rgba(156, 163, 175)' }}
+                    >
                       <i className="fas fa-check-square"></i>
                     </div>
                     <h6 className="text-xl font-semibold">
