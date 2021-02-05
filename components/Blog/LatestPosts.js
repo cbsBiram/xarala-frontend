d2a510d16026ba48268ee29148917927a08981e3
Xarala <xaralaxarala@gmail.com>
2021-02-05 08:48:11 +0000

    Display related post

--- components/Blog/LatestPosts.js
+++ components/Blog/LatestPosts.js
@@ -1,48 +1,72 @@
+import { useQuery } from '@apollo/client'
+import dateformat from 'dateformat'
+import Link from 'next/link'
 import React from 'react'
+import { COURSES_AND_POSTS_QUERY } from '../../utils/constants'
 
 const LatestPosts = () => {
+  const { loading, error, data } = useQuery(COURSES_AND_POSTS_QUERY)
+
+  if (loading) return <h2>Chargement...</h2>
+  const { latestPosts } = data
+
   return (
     <>
       <div className="mt-10 px-8">
         <h1 className="mb-4 text-xl font-bold text-gray-700">
           Récemment publié
         </h1>
-        <div className="flex flex-col bg-white px-8 py-6 max-w-sm mx-auto rounded-lg shadow-md">
-          <div className="flex justify-center items-center">
-            <a
-              href="#"
-              className="px-2 py-1 bg-gray-600 text-sm text-green-100 rounded hover:bg-gray-500"
+
+        {latestPosts &&
+          latestPosts.map((post) => (
+            <div
+              className="flex flex-col bg-white px-8 py-6 max-w-sm mx-auto rounded-lg shadow-md mb-2"
+              key={post.id}
             >
-              Laravel
-            </a>
-          </div>
-          <div className="mt-4">
-            <a
-              href="#"
-              className="text-lg text-gray-700 font-medium hover:underline"
-            >
-              Build Your New Idea with Laravel Freamwork.
-            </a>
-          </div>
-          <div className="flex justify-between items-center mt-4">
-            <div className="flex items-center">
-              <img
-                src="https://images.unsplash.com/photo-1492562080023-ab3db95bfbce?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=731&amp;q=80"
-                alt="avatar"
-                className="w-8 h-8 object-cover rounded-full"
-              />
-              <a
-                href="#"
-                className="text-gray-700 text-sm mx-3 hover:underline"
-              >
-                Alex John
-              </a>
+              <div className="flex justify-center items-center">
+                <a
+                  href="#"
+                  className="px-2 py-1 bg-gray-600 text-sm text-green-100 rounded hover:bg-gray-500"
+                >
+                  {post.tags[0] ? post.tags[0].name : 'Xarala'}
+                </a>
+              </div>
+              <div className="mt-4">
+                <Link as={`/blog/${post.slug}`} passHref href="/blog/[slug]">
+                  <a
+                    href="#"
+                    className="text-lg text-gray-700 font-medium hover:underline"
+                  >
+                    {post.title}
+                  </a>
+                </Link>
+              </div>
+              <div className="flex justify-between items-center mt-4">
+                <div className="flex items-center">
+                  <img
+                    src={
+                      post.author && post.author.avatar
+                        ? `${process.env.MEDIA_URL}${post.author.avatar}`
+                        : require('assets/img/team-1-800x800.jpg')
+                    }
+                    alt={post.title}
+                    className="w-8 h-8 object-cover rounded-full"
+                  />
+                  <a
+                    href="#"
+                    className="text-gray-700 text-sm mx-3 hover:underline"
+                  >
+                    {post.author
+                      ? `${post.author.firstName} ${post.author.lastName}`
+                      : 'Xarala'}
+                  </a>
+                </div>
+                <span className="font-light text-sm text-gray-600">
+                  {dateformat(post.publishDate, 'dd/mm/yyyy')}
+                </span>
+              </div>
             </div>
-            <span className="font-light text-sm text-gray-600">
-              Jun 1, 2020
-            </span>
-          </div>
-        </div>
+          ))}
       </div>
     </>
   )
