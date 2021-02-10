037e6d29748c4a25d522376550cde1e361021503
Ibrahima Biram Diouf <ibrahimabiram@gmail.com>
2021-02-10 20:31:24 +0000

    Feature/admin dashboard

---
+++ pages/search.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
+import Loading from '../components/Shared/Loading'
+import Page from '../layouts/Page'
+import { SEARCH_RESULTS } from '../utils/queries'
+import { useQuery } from '@apollo/client'
+import SearchResults from '../components/Partials/SearchResults'
+
+export default function Search() {
+  const router = useRouter()
+  const { q } = router.query
+  const { loading, data } = useQuery(SEARCH_RESULTS, {
+    variables: { query: q },
+  })
+
+  if (loading) return <Loading />
+
+  return <SearchResults data={data.homepageSearch} query={q} />
+}
+
+Search.layout = Page
