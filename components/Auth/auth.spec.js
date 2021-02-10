86feb6cded2e74a20dd2f7cdb48afb2cd94735d5
Xarala <xaralaxarala@gmail.com>
2021-02-10 20:29:00 +0000

    Remove title spec

--- components/Auth/auth.spec.js
+++ components/Auth/auth.spec.js
@@ -1,11 +1,16 @@
 import { mount } from 'enzyme'
+import { MockedProvider } from '@apollo/client/testing'
 
 import Login from './Login'
 
 /** @test {Title Component} */
 describe('Login Component', () => {
   it('should render without crashing', () => {
-    const wrapper = mount(<Login />)
+    const wrapper = mount(
+      <MockedProvider>
+        <Login />
+      </MockedProvider>
+    )
 
     expect(wrapper.find('h2')).toHaveLength(1)
   })
