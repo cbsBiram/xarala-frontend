0212d6fc98bdf65e418d80d2218fbf29938ee61b
Ibrahima Biram Diouf <ibrahimabiram@gmail.com>
2021-02-11 13:34:17 +0000

    Only students can apply to quiz

---
+++ pages/400.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import ErrorPage from '../components/Pages/400'
+import Page from '../layouts/Page'
+
+const Error400 = () => {
+  return (
+    <>
+      <ErrorPage />
+    </>
+  )
+}
+
+Error400.layout = Page
+
+export default Error400
--- pages/courses/quiz/[slug].js
+++ pages/courses/quiz/[slug].js
@@ -6,12 +6,15 @@ import { useRouter } from 'next/router'
 import Loading from '../../../components/Shared/Loading'
 import Page from '../../../layouts/Page'
 import QuizComponent from '../../../components/Quiz/Quiz'
+import Error400 from '../../400'
 import { SINGLE_QUIZ_QUERY } from '../../../utils/queries'
+import { ME_QUERY } from '../../../utils/constants'
 
 const Quiz = () => {
   const router = useRouter()
   const { slug } = router.query
 
+  const { loading: meLoading, data: meData } = useQuery(ME_QUERY)
   const { loading: quizLoading, errors: quizErrors, data: quizData } = useQuery(
     SINGLE_QUIZ_QUERY,
     {
@@ -20,9 +23,13 @@ const Quiz = () => {
       },
     }
   )
-  const { quiz } = quizData ? quizData : {}
 
-  if (quizLoading) return <Loading />
+  const { quiz } = quizData ? quizData : {}
+  const { me } = meData ? meData : {}
+
+  if (quizLoading || meLoading) return <Loading />
+  if (!me || !me.isStudent) return <Error400 />
+
   return (
     <>
       <NextSeo
